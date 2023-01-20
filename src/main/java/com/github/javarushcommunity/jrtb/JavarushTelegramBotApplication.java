00a27691c4df9aba15a8cf75e58c5d2122bea697
Danil-1007 <logvinov.danil@gmail.com>
2023-01-19 20:42:34 -0500

    JRTB-2: Added stub telegram bot to the project

--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -1,13 +1,24 @@
 package com.github.javarushcommunity.jrtb;
 
+import com.github.javarushcommunity.jrtb.bot.JavarushTelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JavarushTelegramBotApplication.class, args);
+		try {
+			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+			botsApi.registerBot(new JavarushTelegramBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
