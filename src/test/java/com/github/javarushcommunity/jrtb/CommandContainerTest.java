64acfee3f5320c75dc64580e1dac4b6ee5b27827
Danil-1007 <logvinov.danil@gmail.com>
2023-01-27 13:31:10 -0500

    JRTB-3: Added to telegramBot new command

---
+++ src/test/java/com/github/javarushcommunity/jrtb/CommandContainerTest.java
@@ -0,0 +1,48 @@
+package com.github.javarushcommunity.jrtb;
+
+
+import com.github.javarushcommunity.jrtb.command.Command;
+import com.github.javarushcommunity.jrtb.command.CommandContainer;
+import com.github.javarushcommunity.jrtb.command.CommandName;
+import com.github.javarushcommunity.jrtb.command.UnknownCommand;
+import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
+@DisplayName("Unit-level testing for CommandContainer")
+public class CommandContainerTest {
+    private CommandContainer commandContainer;
+
+    @BeforeEach
+    public void init() {
+        SendBotMessageService sendBotMessageService = Mockito.mock(SendBotMessageService.class);
+        commandContainer = new CommandContainer(sendBotMessageService);
+    }
+
+    @Test
+    public void shouldGetAllTheExistingCommands() {
+        //when-then
+        Arrays.stream(CommandName.values())
+                .forEach(commandName -> {
+                    Command command = commandContainer.retrieveCommand(commandName.getCommandName());
+                    Assertions.assertNotEquals(UnknownCommand.class, command.getClass());
+                });
+    }
+
+    @Test
+    public void shouldReturnUnknownCommand() {
+        //given
+        String unknownCommand = "/fgjhdfgdfg";
+
+        //when
+        Command command = commandContainer.retrieveCommand(unknownCommand);
+
+        //then
+        Assertions.assertEquals(UnknownCommand.class, command.getClass());
+    }
+}
